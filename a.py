485eb84a9bd33a595cbf1c5804696d8348c3c414
Quan Yang <quanyang@live.com>
2015-10-30 18:32:10 +0800

    Implementation of PERFECT ENCRYPTION

--- a.py
+++ a.py
@@ -1,6 +1,5 @@
 import binascii
 import os
-from bitstring import BitArray
 
 preSharedKey = os.urandom(64)
 preSharedKeyHex = binascii.b2a_hex(preSharedKey)
@@ -21,3 +20,17 @@ key1 = preSharedKeyInt ^ nonce1Int
 print "key1 : " + '%x' % key1 + "\n"
 key2 = key1 ^ nonce2Int
 print "key2 : " + '%x' % key2 + "\n"
+
+
+secret = file("secretkey","wb")
+secret.write(preSharedKey.encode('hex'))
+secret.close()
+
+nonce1 = file("nonce1","wb")
+nonce1.write(nonce1Hex)
+nonce1.close()
+
+nonce2 = file("nonce2","wb")
+nonce2.write(nonce2Hex)
+nonce2.close()
+
